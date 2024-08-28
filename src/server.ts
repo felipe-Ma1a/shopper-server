55096b2f94676c6786b1eadedca64cd0875b3477
Felipe Maia <felipemaia850@gmail.com>
2024-08-28 17:54:18 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const port = 3333;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error.",
+  });
+});
 
 app.listen(port, () => console.log(`Servidor online na porta ${port}`));
